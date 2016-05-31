dcbc6b6eec842010127050734256f533f7731999
vagrant <vagrant@homestead>
2016-05-31 16:09:01 +0000

    gulp and elixr in project

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,22 @@
-/*
 var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+
+	var sass_files = [
+		'test',
+		'test2'
+	]
+
+	var css_destination = 'public/assets/css/'
+
+
+
+
+
+	for (var file of sass_files) {
+		mix.sass(`${file}.scss`, `${css_destination}${file}.css` )
+	}
 });
-*/
-var fs = require('fs');
-var gulp = require('gulp-param')(require('gulp'), process.argv);
-var ffmpeg = require('gulp-fluent-ffmpeg');
- 
-gulp.task('valid', function(id) {
 
-	var path = __dirname + '/public/users_content/videos/' + id;
 
-	fs.readdir(path, function(err, files) {
-		var file = files[0];
-		var extension = file.slice(-3);
-		var file_no_extension = file.substr(0, file.lastIndexOf('.'));
-
-		if (extension == 'mp4' || extension || 'avi') {
-				
-			return gulp.src(path + '/' + file)
-				.pipe(ffmpeg('avi'), function(cmd) {
-					return cmd.output('toto.avi')
-				})
-				.pipe(gulp.dest(path));
-		}
-	});
-});
 
